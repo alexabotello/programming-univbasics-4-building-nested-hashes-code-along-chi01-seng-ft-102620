f3b6df87311dae63edfcf56654d2b1d9c7567a4a
Alexa Botello <abotello20@yahoo.com>
2020-10-18 20:41:57 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,21 +1,24 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-    },
+    epic_tragedy = {
+   :montague => {
       :patriarch => {
-    },
+        },
       :matriarch => {
-    },
+        },
       :hero => {
-    
+        },
       :hero_friends => []
-      }
-      }
-      }
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+   }, 
+   :capulet => {
+      :patriarch => {
+        },
+      :matriarch => {
+        },
+      :heroine => {
+        },
+      :heroine_friends => []
+   }
+  }
 
   
 end
